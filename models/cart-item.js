354c953c71e616559cec584fae4def01a7d3ffb2
bastej <siatkasebastian@gmail.com>
2020-01-14 21:58:11 +0100

    Creating cart item model

---
+++ models/cart-item.js
@@ -0,0 +1,9 @@
+class CartItem {
+  constructor(key, quantity, productPrice, productTitle, sum) {
+    this.key = key;
+    this.quantity = quantity;
+    this.productPrice = productPrice;
+    this.productTitle = productTitle;
+    this.sum = sum;
+  }
+}
