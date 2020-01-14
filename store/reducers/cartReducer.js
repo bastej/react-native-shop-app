61b10c8f67864a082a987f0c565095e62364da02
bastej <siatkasebastian@gmail.com>
2020-01-14 21:58:34 +0100

    Adding reducer and actions for cart

---
+++ store/reducers/cartReducer.js
@@ -0,0 +1,48 @@
+import { ADD_TO_CART } from "../actions/cart";
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART: {
+      const newProduct = action.payload;
+      const productId = newProduct.id;
+      const productPrice = newProduct.price;
+      const productTitle = newProduct.title;
+
+      let currentProduct;
+
+      if (state.items[productId]) {
+        const existingProduct = state.items[productId];
+        currentProduct = new CartItem(
+          existingProduct.quantity + 1,
+          productPrice,
+          productTitle,
+          existingProduct.sum + productPrice
+        );
+      } else {
+        currentProduct = new CartItem(
+          productId,
+          1,
+          productPrice,
+          productTitle,
+          productPrice
+        );
+      }
+      return {
+        ...state,
+        items: {
+          ...state.items,
+          [productId]: currentProduct,
+        },
+        totalAmount: state.totalAmount + productPrice,
+      };
+    }
+  }
+  return state;
+};
+
+export default reducer;
